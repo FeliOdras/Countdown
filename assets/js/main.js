d80181d1656acbe77564dc22c5fd34068dd463af
FeliOdras <ariane.feldmann@odras.de>
2019-03-04 10:39:37 +0100

    Created end date output

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
 class Countdown {
-    constructor(domselector, endDate) {
+    constructor(domselector, year, month, day, hour, minute) {
         this.htmlContainer = document.querySelector(domselector);
-        this.endDate = endDate;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
         this.showEndDate()
     }
 
     showEndDate() {
-        console.log(this.endDate)
+        let endDate = `${this.year}-${this.month}-${this.day} ${this.hour}:${this.minute}`;
+        console.log(endDate);
     }
-
 }
-
-const currentCountdown = new Countdown('#countdown', '2019-04-23T:23:42')
+const currentCountdown = new Countdown('#countdown', '2020', '04', '23', '23', '42')
